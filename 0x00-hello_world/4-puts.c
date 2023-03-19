799f196be99e4a78ef8c455f1facb00b5bc01441
mamusha <abrarawteshager@gmail.com>
2023-03-19 06:29:37 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - prints "programming is like building a multilingual puzzle followed by a new line 
- * Return: Always 0.
+ * main - prints "programming is like building a multilingual puzzle,
+ * followed by a new line
+ * Return:Always o.
  */
 
 int main(void)
-
+	
 {
-	puts("\" programming is like building a multilingual puzzle,\n");
-	return (0);
+
+puts("\"programming is like building a multilingual puzzle.");
+return (0);
 }

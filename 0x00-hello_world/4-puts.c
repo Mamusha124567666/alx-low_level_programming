2c101c5ad21bc1d3dad1ac5057860128c75cb6e2
mamusha <abrarawteshager@gmail.com>
2023-03-19 00:33:28 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints "programming is like building a multilingual puzzle followed by a new line 
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	puts("\" programming is like building a multilingual puzzle,\n");
+	return (0);
+}

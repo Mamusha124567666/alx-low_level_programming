0b3e2c5992d97563d8cd61fa4c926fef5b3eaf2a
mamusha <abrarawteshager@gmail.com>
2023-04-04 23:29:35 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - Main Entry
+ * @s: input
+ */
+void _puts_recursion(char *g)
+{
+	if (*(g) != '\0')
+	{
+		_putchar(*g);
+		_puts_recursion(g + 1);
+	}
+	if (*(g) == '\0')
+	{
+		_putchar('\n');
+	}
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * File: main
+ *
+ * Desc: header file
+ */
+void _puts_recursion(char *g);
+int _putchar(char c);
+void _print_rev_recursion(char *g);
+int _strlen_recursion(char *g);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *g);
+int wildcmp(char *g1, char *g2);
+
+#endif

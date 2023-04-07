6511bcc2497a829bddd1f77a2701364c1f7e9457
mamusha <abrarawteshager@gmail.com>
2023-04-07 03:36:21 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,16 +8,16 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, res = 3;
+	int i, res = 1;
 
-	if (argc != 6)
+	if (argc != 3)
 	{
 		printf("%s\n", "Error");
 		return (1);
 	}
 	else
 	{
-		for (i = 3; i < argc; i++)
+		for (i = 1; i < argc; i++)
 		{
 			res *= atoi(argv[i]);
 		}

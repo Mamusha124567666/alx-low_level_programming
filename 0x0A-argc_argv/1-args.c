978d6b0be65779ab7e2741b76531f4223c9d3df7
mamusha <abrarawteshager@gmail.com>
2023-04-07 03:30:27 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,8 +3,8 @@
  * main - prints the number of arguments that are passed to it
  * @argc: input
  * @argv: input
- * Return: Always 0 
-  */
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 	(void)argv;

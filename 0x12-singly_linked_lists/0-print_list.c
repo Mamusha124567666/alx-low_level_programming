c8aede3a0556be558a7e631f9714a675f1c73776
mamusha <abrarawteshager@gmail.com>
2023-04-28 09:06:07 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+*@len : length.
+* @str: string
+*/
+size_t print_list(const list_t *h);
+
+size_t node *h, *len, *str;
+{
+h->next = len;
+len->next = str;
+str->next = last;
+last->next = NULL;
+size_t node *temp = h;
+
+if(temp==NULL)
+
+{printf("[0] (nil)\n");
+}
+else
+printf("[%d] %s\n", h->len, h->str);
+
+return 0;
+}
